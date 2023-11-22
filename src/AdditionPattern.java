a9af8525441bbb63a23fa78dcfae310833a3e4a1
BT_2N2_08 <sample@bths.edu>
2023-11-22 13:50:43 -0500

    1:50 pm, 11/22/2023

--- src/AdditionPattern.java
+++ src/AdditionPattern.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class AdditionPattern {
+public class AdditionPattern {
+    private int num;
+    private int add;
+    private int start;
+
+    public AdditionPattern(int num, int add) {
+        this.num = num;
+        this.add = add;
+        start = num;
+    }
+
+    public int currentNumber() {
+        return num;
+    }
+
+    public void next() {
+        num += add;
+    }
+
+    public void prev() {
+        int thing = num - add;
+        if (thing >= start) {
+            num -= add;
+        }
+
+    }
 }
+
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class StepTracker {
+public class StepTracker {
+
+    private int min;
+    private int activeDays;
+    private int totalSteps;
+    private int days;
+
+
+    public StepTracker(int min) {
+        this.min = min;
+    }
+
+    public void addDailySteps(int add) {
+		if (add >= min) {
+        activeDays++;
+    }
+    totalSteps += add;
+    days++;
+}
+
+    public int activeDays() {
+        return activeDays;
+    }
+
+    public double averageSteps() {
+        if (days == 0) {
+            return 0.0;
+        }
+        return (double) totalSteps / days;
+    }
 }
